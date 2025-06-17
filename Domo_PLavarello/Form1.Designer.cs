52dc44c1f0307fca03e57fa5b8dc62181c6461f4
Victoria Lavarello <vl2023077281@virtual.upt.pe>
2025-06-17 12:00:22 -0500

    Conexion MQTT

--- Domo_PLavarello/Form1.Designer.cs
+++ Domo_PLavarello/Form1.Designer.cs
@@ -28,38 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblEstado1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 31);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Domo Demonio";
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(71, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(201, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Domo Demonio";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 62);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(61, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "MQTT:";
+            // 
+            // lblEstado1
+            // 
+            this.lblEstado1.AutoSize = true;
+            this.lblEstado1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado1.Location = new System.Drawing.Point(63, 46);
+            this.lblEstado1.Name = "lblEstado1";
+            this.lblEstado1.Size = new System.Drawing.Size(51, 20);
+            this.lblEstado1.TabIndex = 2;
+            this.lblEstado1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(332, 416);
+            this.Controls.Add(this.lblEstado1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Domo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,8 +84,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEstado1;
     }
 }
 
--- Domo_PLavarello/Form1.cs
+++ Domo_PLavarello/Form1.cs
@@ -1,33 +1,86 @@
 ﻿using System;
+using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace Domo_PLavarello {
     public partial class Form1 : Form {
         static MqttClient cliente;
+        string mqtt_server = "broker.hivemq.com";
+        int mqtt_port = 1883;
+        string mqtt_user = null;
+        string mqtt_pass = null;
+
+
         public Form1() {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            string mqtt_server = "broker.hivemq.com";
-            string mqtt_port = "1883";
-            string mqtt_user = null;
-            string mqtt_pass = null;
+            ConectarMQTT();
+        }
 
+        private void ConectarMQTT() {
             try {
-                cliente = new MqttClient(mqtt_server, int.Parse(mqtt_port), false, MqttSslProtocols.None, null, null);
+                if (cliente != null && cliente.IsConnected) {
+                    cliente.Disconnect();
+                }
+
+                lblEstado1.Text = "Conectando";
+                lblEstado1.ForeColor = Color.Yellow;
+
+                cliente = new MqttClient(mqtt_server, mqtt_port, false, MqttSslProtocols.None, null, null);
                 cliente.ProtocolVersion = MqttProtocolVersion.Version_3_1;
-                byte conexion = cliente.Connect("Chamuco", mqtt_user, mqtt_pass);
+
+                byte conexion = cliente.Connect("DomoDemonio", mqtt_user, mqtt_pass);
+
                 if (conexion == 0) {
-                    MessageBox.Show("MQTT Conectado");
+                    lblEstado1.Text = "Conectado";
+                    lblEstado1.ForeColor = Color.Green;
+                    // registramos el callback
+                    cliente.MqttMsgPublishReceived += MQTT_mensajeRecivido;
+                    // nos suscribimos a los topics
+                    string[] topics = new string[1];
+                    topics[0] = "domo/domodata";
+                    byte[] msg = new byte[1];
+                    msg[0] = MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE;
+
+                    cliente.Subscribe(topics, msg);
+
                 } else {
-                    MessageBox.Show("MQTT Desconectado");
+                    lblEstado1.Text = "Desconectado";
+                    lblEstado1.ForeColor = Color.Red;
                 }
             } catch (Exception ex) {
-                MessageBox.Show(ex.Message);
-                //TODO: manejar la excepcion
+                lblEstado1.Text = "Error de conexión";
+                lblEstado1.ForeColor = Color.OrangeRed;
+                MessageBox.Show("Error al conectar: " + ex.Message);
             }
+        }
+
+        private void DesconectarMQTT() {
+            if (cliente != null && cliente.IsConnected) {
+                lblEstado1.Text = "Desconectado";
+                lblEstado1.ForeColor = Color.Red;
+                cliente.Disconnect();
+                cliente = null;
+            }
+        }
+
+        private void MQTT_mensajeRecivido(object sender, MqttMsgPublishEventArgs e) {
+            string topic = e.Topic;
+            string value = Encoding.UTF8.GetString(e.Message);
+            //todo: manejar el paquete y guardar los datos
+            string mensaje = "Topic: " + topic + " | Valor: " + value;
+            MessageBox.Show(mensaje);
+        }
+
+        // cuando se cierra el formulario
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            // desconectamos del servidor MQTT
+            DesconectarMQTT();
         }
     }
 }
